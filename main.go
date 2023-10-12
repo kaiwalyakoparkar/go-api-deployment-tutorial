42a48617e4701da91bbe9bfc80bc4ffa7c16e6a0
Kaiwalya Koparkar <kaiwalyakoparkar@gmail.com>
2023-10-12 07:38:28 +0000

    feat: Go api added

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+
+	fmt.Println("Welcome to the Ping-Pong API")
+
+	//Initializing the gin package
+	router := gin.Default()
+
+	//Endpoint we are going to use today
+	router.GET("/ping", handlePing)
+
+	//Server runs on everywhere IP and on 8080 port
+	router.Run("0.0.0.0:8080")
+}
+
+//Function to handle the /ping endpoint
+func handlePing(g *gin.Context) {
+	g.IndentedJSON(http.StatusOK, gin.H{"message": "Pong!"})
+}
